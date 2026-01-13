3475339aaa9e0217c4460bd9f8d0147d6d8647b5
taku <takumi.f.0308@icloud.com>
2026-01-13 23:57:04 +0900

    共通バリデーション

---
+++ pkg/validation/string.go
@@ -0,0 +1,62 @@
+package validation
+
+import (
+	"fmt"
+	"strings"
+)
+
+// NotEmpty validates that a string value is not empty
+func NotEmpty(value, fieldName string) error {
+	if strings.TrimSpace(value) == "" {
+		return fmt.Errorf("%s cannot be empty", fieldName)
+	}
+	return nil
+}
+
+// MaxLength validates that a string value does not exceed the maximum length
+func MaxLength(value string, max int, fieldName string) error {
+	if len(value) > max {
+		return fmt.Errorf("%s は%d文字以内でなければなりません", fieldName, max)
+	}
+	return nil
+}
+
+// MinLength validates that a string value meets the minimum length
+func MinLength(value string, min int, fieldName string) error {
+	if len(value) < min {
+		return fmt.Errorf("%s は%d文字以上でなければなりません", fieldName, min)
+	}
+	return nil
+}
+
+// Length validates that a string value is within a specific length range
+func Length(value string, min, max int, fieldName string) error {
+	if err := MinLength(value, min, fieldName); err != nil {
+		return err
+	}
+	if err := MaxLength(value, max, fieldName); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NotEmptyAndMaxLength is a convenience function combining NotEmpty and MaxLength
+func NotEmptyAndMaxLength(value string, max int, fieldName string) error {
+	if err := NotEmpty(value, fieldName); err != nil {
+		return err
+	}
+	if err := MaxLength(value, max, fieldName); err != nil {
+		return err
+	}
+	return nil
+}
+
+// OneOf validates that a string value is one of the allowed options
+func OneOf(value string, allowedValues []string, fieldName string) error {
+	for _, allowed := range allowedValues {
+		if value == allowed {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s は許可された値のいずれかでなければなりません", fieldName)
+}
