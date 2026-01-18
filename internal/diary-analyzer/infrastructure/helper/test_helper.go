f793c36aca0bfe83e67b2e941f5e8bb821060239
taku <takumi.f.0308@icloud.com>
2026-01-19 00:14:00 +0900

    test軍

---
+++ internal/diary-analyzer/infrastructure/helper/test_helper.go
@@ -0,0 +1,47 @@
+package helper
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"gorm.io/gorm"
+
+	"github.com/furuya-3150/fam-diary-log/internal/diary-analyzer/infrastructure/config"
+	"github.com/furuya-3150/fam-diary-log/pkg/db"
+	"github.com/joho/godotenv"
+)
+
+const (
+	DB_USER        = "postgres"
+	DB_PASSWORD    = "password"
+	DB_NAME        = "test_diary"
+	DB_HOST        = "db"
+	DB_PORT        = "5432"
+	DB_TIMEOUT_SEC = "5"
+	DB_SSLMODE     = "disable"
+)
+
+// setup
+func SetupTestDB(t *testing.T) (*db.DBManager) {
+	if os.Getenv("GO_ENV") == "dev" {
+		_ = godotenv.Load("../../.env")
+	}
+	config := config.Load()
+	fmt.Println(config.DB.TestDatabaseURL)
+	dbManager := db.NewDBManager(config.DB.TestDatabaseURL)
+
+	// cleanup
+	if err := dbManager.GetGorm().Exec("DELETE FROM diary_analyses").Error; err != nil {
+		t.Fatalf("failed to clean up test database: %v", err)
+	}
+
+	return dbManager
+}
+
+// teardown
+func TeardownTestDB(t *testing.T, gormDB *gorm.DB) {
+	if err := gormDB.Exec("DELETE FROM diary_analyses").Error; err != nil {
+		t.Logf("warning: failed to cleanup test database: %v", err)
+	}
+}
