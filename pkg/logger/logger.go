e05bad7385303212d058079ed49d80475edacd10
taku <takumi.f.0308@icloud.com>
2026-02-18 20:52:14 +0900

    タイムゾーンを東京へ

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,11 +3,25 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"time"
 )
 
 func New(level slog.Level) *slog.Logger {
+	// 日本時間（JST）を設定
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+	}
+
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: level,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				t := a.Value.Time()
+				a.Value = slog.TimeValue(t.In(jst))
+			}
+			return a
+		},
 	})
 
 	return slog.New(handler)
